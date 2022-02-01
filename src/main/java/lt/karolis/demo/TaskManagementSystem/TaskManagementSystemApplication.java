384b33f1d8e8ba9c8e56e2044a15e5916018f330
karolisas <karolisas@gmail.com>
2022-02-01 09:24:34 +0200

    module gradle file dependencies moved to parent

--- src/main/java/lt/karolis/demo/TaskManagementSystem/TaskManagementSystemApplication.java
+++ src/main/java/lt/karolis/demo/TaskManagementSystem/TaskManagementSystemApplication.java
@@ -3,16 +3,18 @@ package lt.karolis.demo.TaskManagementSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan (basePackages = "lt.karolis.demo")
+@ComponentScan (basePackages = "lt.karolis")
+@EnableJpaRepositories("lt.karolis")
 public class TaskManagementSystemApplication {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(TaskManagementSystemApplication.class, args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystemApplication.class, args);
+	}
 
 }
